30b5f8f541413f31d211c5fadb8342fe93ff0459
draganmajkic <dragan.majkic@uns.ac.rs>
2021-06-07 00:05:44 +0200

    Netlify config

--- FrontendRVA/src/app/app.constants.ts
+++ FrontendRVA/src/app/app.constants.ts
@@ -1,5 +1,15 @@
+/*
 export const KLIJENT_URL = 'http://localhost:8083/klijent';
 export const KREDIT_URL = 'http://localhost:8083/kredit';
 export const TIP_RACUNA_URL = 'http://localhost:8083/tipRacuna';
 export const RACUN_URL = 'http://localhost:8083/racun';
 export const RACUNI_KLIJENTA_URL = 'http://localhost:8083/racuniKlijenta';
+*/
+
+export const KLIJENT_URL = 'https://majkic-rva-backend.herokuapp.com/klijent';
+export const KREDIT_URL = 'https://majkic-rva-backend.herokuapp.com/kredit';
+export const TIP_RACUNA_URL = 'https://majkic-rva-backend.herokuapp.com/tipRacuna';
+export const RACUN_URL = 'https://majkic-rva-backend.herokuapp.com/racun';
+export const RACUNI_KLIJENTA_URL = 'https://majkic-rva-backend.herokuapp.com/racuniKlijenta';
+
+//https://majkic-rva-backend.herokuapp.com/
